e75108856219e8d9688105ea57b403f809b6a187
ghorsington <neigh@coder.horse>
2021-06-13 09:47:40 +0300

    Fix Il2CppImage struct for Il2Cpp version 27

--- UnhollowerBaseLib/Runtime/VersionSpecific/Image/Images_27.cs
+++ UnhollowerBaseLib/Runtime/VersionSpecific/Image/Images_27.cs
@@ -8,9 +8,15 @@ namespace UnhollowerBaseLib.Runtime.VersionSpecific.Image
     {
         public INativeImageStruct CreateNewImageStruct()
         {
-            var pointer = Marshal.AllocHGlobal(Marshal.SizeOf<Il2CppImageU2019>());
+            var pointer = Marshal.AllocHGlobal(Marshal.SizeOf<Il2CppImageU2020_2>());
+            var imageMetadata =
+                (Il2CppImageGlobalMetadata*) Marshal.AllocHGlobal(Marshal.SizeOf<Il2CppImageGlobalMetadata>());
 
-            *(Il2CppImageU2019*)pointer = default;
+            *(Il2CppImageU2020_2*) pointer = default;
+            *imageMetadata = default;
+
+            imageMetadata->image = (Il2CppImage*) pointer;
+            ((Il2CppImageU2020_2*) pointer)->metadataHandle = imageMetadata;
 
             return new NativeImageStruct(pointer);
         }
@@ -21,24 +27,35 @@ namespace UnhollowerBaseLib.Runtime.VersionSpecific.Image
         }
 
         [StructLayout(LayoutKind.Sequential)]
-        private struct Il2CppImageU2019
+        private struct Il2CppImageU2020_2
         {
-            public IntPtr name; // const char*
+            public IntPtr name;      // const char*
             public IntPtr nameNoExt; // const char*
             public Il2CppAssembly* assembly;
 
             public uint typeCount;
 
             public uint exportedTypeCount;
+
             public uint customAttributeCount;
 
-            public IntPtr metadataHandle;
+            public /*Il2CppNameToTypeDefinitionIndexHashTable **/ Il2CppImageGlobalMetadata* metadataHandle;
 
             public /*Il2CppNameToTypeDefinitionIndexHashTable **/ IntPtr nameToClassHashTable;
-            public IntPtr codeGenModule;
+
+            public /*Il2CppCodeGenModule*/ IntPtr codeGenModule;
 
             public uint token;
             public byte dynamic;
+        }
+        
+        private struct Il2CppImageGlobalMetadata
+        {
+            public int typeStart;
+            public int exportedTypeStart;
+            public int customAttributeStart;
+            public int entryPointIndex;
+            public Il2CppImage* image;
         }
 
         private class NativeImageStruct : INativeImageStruct
@@ -50,15 +67,16 @@ namespace UnhollowerBaseLib.Runtime.VersionSpecific.Image
 
             public IntPtr Pointer { get; }
 
-            public Il2CppImage* ImagePointer => (Il2CppImage*)Pointer;
+            public Il2CppImage* ImagePointer => (Il2CppImage*) Pointer;
 
-            private Il2CppImageU2019* NativeImage => (Il2CppImageU2019*)ImagePointer;
+            private Il2CppImageU2020_2* NativeImage => (Il2CppImageU2020_2*) ImagePointer;
 
             public ref Il2CppAssembly* Assembly => ref NativeImage->assembly;
 
             public ref byte Dynamic => ref NativeImage->dynamic;
 
             public ref IntPtr Name => ref NativeImage->name;
+            
             public bool HasNameNoExt => true;
 
             public ref IntPtr NameNoExt => ref NativeImage->nameNoExt;
