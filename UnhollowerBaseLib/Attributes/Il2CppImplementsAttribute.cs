b46e23add3f73cea200e3e536191cf7c2531e5eb
PHPCore <phpcore2.0@gmail.com>
2021-11-18 14:31:30 +0100

    Update style

--- UnhollowerBaseLib/Attributes/Il2CppImplementsAttribute.cs
+++ UnhollowerBaseLib/Attributes/Il2CppImplementsAttribute.cs
@@ -5,7 +5,7 @@ namespace UnhollowerBaseLib.Attributes
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class Il2CppImplementsAttribute : Attribute
     {
-        public readonly Type[] Interfaces;
+        public Type[] Interfaces { get; private init; }
 
         public Il2CppImplementsAttribute(params Type[] interfaces)
         {
