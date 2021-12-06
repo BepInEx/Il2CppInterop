8151774b9a1b062ad088f8b6217824dbacd2429b
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2021-12-07 00:12:54 +0200

    Better Class::Init search method

---
+++ UnhollowerBaseLib/MemoryUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace UnhollowerBaseLib
+{
+    internal class MemoryUtils
+    {
+        public static unsafe void* FindSignatureInBlock(void* block, long blockSize, string pattern, string mask, long sigOffset = 0)
+            => FindSignatureInBlock(block, blockSize, pattern.ToCharArray(), mask.ToCharArray(), sigOffset);
+        public static unsafe void* FindSignatureInBlock(void* block, long blockSize, char[] pattern, char[] mask, long sigOffset = 0)
+        {
+            for (long address = 0; address < blockSize; address++)
+            {
+                bool found = true;
+                for (uint offset = 0; offset < mask.Length; offset++)
+                {
+                    if (((*(byte*)(address + (long)block + offset)) != (byte)pattern[offset]) && mask[offset] != '?')
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found)
+                    return (void*)(address + (long)block + sigOffset);
+            }
+            return (void*)0;
+        }
+    }
+}
