c2f27a7c096f0a60d15a238c125bc63b9f1dc9b4
ghorsington <neigh@coder.horse>
2022-05-08 22:19:23 +0300

    Replace corlib ref rewriting with a simpler temporary hack
    Fixes issue with System.Private.Corlib being referenced

--- Il2CppInterop.Generator/CorlibReferences.cs
+++ Il2CppInterop.Generator/CorlibReferences.cs
@@ -1,233 +1,217 @@
 using System;
-using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
-using Il2CppInterop.Generator.MetadataAccess;
+using System.Runtime.CompilerServices;
 using Mono.Cecil;
-using Mono.Cecil.Rocks;
 
-namespace Il2CppInterop.Generator
+namespace Il2CppInterop.Generator;
+
+// TODO: Instead of a cecil hack, replace with automatically generated code (do same for Il2CppInterop.Runtime)
+internal static class CorlibReferences
 {
-    public static class CorlibReferences
+    private static AssemblyNameReference GetCoreLibraryReference(ModuleDefinition module)
     {
-        internal enum ElementType : byte
+        return module.TypeSystem.CoreLibrary as AssemblyNameReference;
+    }
+
+    public static void RewriteReferenceToMscorlib(AssemblyNameReference assemblyNameReference)
+    {
+        assemblyNameReference.Name = "mscorlib";
+        assemblyNameReference.Version = new Version(4, 0, 0, 0);
+        assemblyNameReference.PublicKeyToken = new byte[] { 183, 122, 92, 86, 25, 52, 224, 137 };
+        assemblyNameReference.Culture = "";
+    }
+
+    public static TypeReference ImportCorlibReference(this ModuleDefinition module, string @namespace, string type)
+    {
+        return module.ImportReference(typeof(string).Assembly.GetType($"{@namespace}.{type}"));
+    }
+
+    public static TypeReference Void(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(void));
+    }
+
+    public static TypeReference Bool(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(bool));
+    }
+
+    public static TypeReference IntPtr(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(IntPtr));
+    }
+
+    public static TypeReference String(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(string));
+    }
+
+    public static TypeReference Int(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(int));
+    }
+
+    public static TypeReference Long(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(long));
+    }
+
+    public static TypeReference Type(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(Type));
+    }
+
+    public static TypeReference Object(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(object));
+    }
+
+    public static TypeReference Enum(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(Enum));
+    }
+
+    public static TypeReference ValueType(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(ValueType));
+    }
+
+    public static TypeReference Delegate(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(Delegate));
+    }
+
+    public static TypeReference MulticastDelegate(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(MulticastDelegate));
+    }
+
+    public static TypeReference DefaultMemberAttribute(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(DefaultMemberAttribute));
+    }
+
+    public static TypeReference NotSupportedException(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(NotSupportedException));
+    }
+
+    public static TypeReference FlagsAttribute(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(FlagsAttribute));
+    }
+
+    public static TypeReference ObsoleteAttribute(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(ObsoleteAttribute));
+    }
+
+    public static TypeReference Attribute(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(Attribute));
+    }
+
+    public static TypeReference RuntimeTypeHandle(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(RuntimeTypeHandle));
+    }
+
+    public static TypeReference ExtensionAttribute(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(ExtensionAttribute));
+    }
+
+    public static TypeReference ParamArrayAttribute(this ModuleDefinition module)
+    {
+        return module.ImportReference(typeof(ParamArrayAttribute));
+    }
+
+    public static TypeReference Action(this ModuleDefinition module, int n = 0)
+    {
+        return n switch
         {
-            Void = 1,
-            Boolean = 2,
-            I4 = 8,
-            I8 = 10,
-            String = 14,
-            I = 24,
-            Object = 28,
-        }
+            0 => module.ImportCorlibReference("System", "Action"),
+            1 => module.ImportCorlibReference("System", "Action`1"),
+            _ => module.ImportCorlibReference("System", $"Action`{n}")
+        };
+    }
 
-        private static Action<TypeReference, ElementType> setEType;
-
-        static CorlibReferences()
+    public static TypeReference Func(this ModuleDefinition module, int n = 0)
+    {
+        return n switch
         {
-            var dm = new DynamicMethod(nameof(setEType), typeof(void), new[] { typeof(TypeReference), typeof(ElementType) },
-                typeof(CorlibReferences), true);
-            var il = dm.GetILGenerator();
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Ldarg_1);
-            il.Emit(OpCodes.Stfld, typeof(TypeReference).GetField("etype", BindingFlags.Instance | BindingFlags.NonPublic));
-            il.Emit(OpCodes.Ret);
-            setEType = (Action<TypeReference, ElementType>)dm.CreateDelegate(typeof(Action<TypeReference, ElementType>));
-        }
+            0 => module.ImportCorlibReference("System", "Func`1"),
+            1 => module.ImportCorlibReference("System", "Func`2"),
+            _ => module.ImportCorlibReference("System", $"Func`{n}")
+        };
+    }
 
-        private static AssemblyNameReference GetCoreLibraryReference(ModuleDefinition module)
+    public static MethodReference TypeGetTypeFromHandle(this ModuleDefinition module)
+    {
+        var type = module.Type();
+        var mr = new MethodReference("GetTypeFromHandle", type, type)
         {
-            var corlib = module.AssemblyReferences.FirstOrDefault(ar => ar.Name is "mscorlib" or "netstandard" or "System.Runtime" or "System.Private.CoreLib");
-            if (corlib is not null)
-                return corlib;
-            corlib = new AssemblyNameReference("mscorlib", new Version(4, 0, 0, 0))
-            {
-                PublicKeyToken = new byte[] { 183, 122, 92, 86, 25, 52, 224, 137 }
-            };
-            module.AssemblyReferences.Add(corlib);
-            return corlib;
-        }
+            HasThis = false
+        };
+        mr.Parameters.Add(new ParameterDefinition(module.RuntimeTypeHandle()));
+        return mr;
+    }
 
-        public static TypeReference ImportCorlibReference(this ModuleDefinition module, string @namespace, string @type)
+    public static MethodReference TypeGetIsValueType(this ModuleDefinition module)
+    {
+        var type = module.Type();
+        var mr = new MethodReference("get_IsValueType", module.Bool(), type)
         {
-            return new(@namespace, @type, module, GetCoreLibraryReference(module));
-        }
+            HasThis = true
+        };
+        return mr;
+    }
 
-        public static TypeReference ImportCorlibReference(this ModuleDefinition module, string @namespace, string @type, string[] genericParams)
+    public static MethodReference TypeGetFullName(this ModuleDefinition module)
+    {
+        var type = module.Type();
+        var mr = new MethodReference("get_FullName", module.String(), type)
         {
-            var typeRef = module.ImportCorlibReference(@namespace, @type);
-            foreach (var genericParam in genericParams)
-                typeRef.GenericParameters.Add(new(genericParam, typeRef));
-            return typeRef;
-        }
+            HasThis = true
+        };
+        return mr;
+    }
 
-
-        public static TypeReference Void(this ModuleDefinition module)
+    public static MethodReference StringEquals(this ModuleDefinition module)
+    {
+        var mr = new MethodReference("Equals", module.Bool(), module.String())
         {
-            var res = new TypeReference("System", "Void", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.Void);
-            return res;
-        }
+            HasThis = false
+        };
+        mr.Parameters.Add(new ParameterDefinition(module.String()));
+        mr.Parameters.Add(new ParameterDefinition(module.String()));
+        return mr;
+    }
 
-        public static TypeReference Bool(this ModuleDefinition module)
-        {
-            var res = new TypeReference("System", "Boolean", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.Boolean);
-            res.IsValueType = true;
-            return res;
-        }
+    public static MethodReference ExtensionAttributeCtor(this ModuleDefinition module)
+    {
+        return new MethodReference(".ctor", module.Void(), module.ExtensionAttribute()) { HasThis = true };
+    }
 
-        public static TypeReference IntPtr(this ModuleDefinition module)
-        {
-            var res = new TypeReference("System", "IntPtr", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.I);
-            res.IsValueType = true;
-            return res;
-        }
+    public static MethodReference ParamArrayAttributeCtor(this ModuleDefinition module)
+    {
+        return new MethodReference(".ctor", module.Void(), module.ParamArrayAttribute()) { HasThis = true };
+    }
 
-        public static TypeReference String(this ModuleDefinition module)
-        {
-            var res = new TypeReference("System", "String", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.String);
-            return res;
-        }
+    public static MethodReference FlagsAttributeCtor(this ModuleDefinition module)
+    {
+        return new MethodReference(".ctor", module.Void(), module.FlagsAttribute()) { HasThis = true };
+    }
 
-        public static TypeReference Int(this ModuleDefinition module)
-        {
-            var res = new TypeReference("System", "Int32", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.I4);
-            res.IsValueType = true;
-            return res;
-        }
+    public static MethodReference NotSupportedExceptionCtor(this ModuleDefinition module)
+    {
+        return new MethodReference(".ctor", module.Void(), module.NotSupportedException())
+        { HasThis = true, Parameters = { new ParameterDefinition(module.String()) } };
+    }
 
-        public static TypeReference Long(this ModuleDefinition module)
-        {
-            var res = new TypeReference("System", "Int64", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.I8);
-            res.IsValueType = true;
-            return res;
-        }
-
-        public static TypeReference Type(this ModuleDefinition module) => new("System", "Type", module, GetCoreLibraryReference(module));
-
-        public static TypeReference Object(this ModuleDefinition module)
-        {
-            var res = new TypeReference("System", "Object", module, GetCoreLibraryReference(module));
-            setEType(res, ElementType.Object);
-            return res;
-        }
-
-        public static TypeReference Enum(this ModuleDefinition module) => new("System", "Enum", module, GetCoreLibraryReference(module));
-
-        public static TypeReference ValueType(this ModuleDefinition module) => new("System", "ValueType", module, GetCoreLibraryReference(module));
-
-        public static TypeReference Delegate(this ModuleDefinition module) => new("System", "Delegate", module, GetCoreLibraryReference(module));
-
-        public static TypeReference MulticastDelegate(this ModuleDefinition module) => new("System", "MulticastDelegate", module, GetCoreLibraryReference(module));
-
-        public static TypeReference DefaultMemberAttribute(this ModuleDefinition module) => new("System.Reflection", "DefaultMemberAttribute", module, GetCoreLibraryReference(module));
-
-        public static TypeReference NotSupportedException(this ModuleDefinition module) => new("System", "NotSupportedException", module, GetCoreLibraryReference(module));
-
-        public static TypeReference FlagsAttribute(this ModuleDefinition module) => new("System", "FlagsAttribute", module, GetCoreLibraryReference(module));
-
-        public static TypeReference ObsoleteAttribute(this ModuleDefinition module) => new("System", "ObsoleteAttribute", module, GetCoreLibraryReference(module));
-
-        public static TypeReference Attribute(this ModuleDefinition module) => new("System", "Attribute", module, GetCoreLibraryReference(module));
-
-        public static TypeReference RuntimeTypeHandle(this ModuleDefinition module) => new("System", "RuntimeTypeHandle", module, GetCoreLibraryReference(module));
-
-        public static TypeReference ExtensionAttribute(this ModuleDefinition module) => new("System.Runtime.CompilerServices", "ExtensionAttribute", module, GetCoreLibraryReference(module));
-
-        public static TypeReference ParamArrayAttribute(this ModuleDefinition module) => new("System", "ParamArrayAttribute", module, GetCoreLibraryReference(module));
-
-        public static TypeReference Action(this ModuleDefinition module, int n = 0)
-        {
-            return n switch
-            {
-                0 => module.ImportCorlibReference("System", "Action"),
-                1 => module.ImportCorlibReference("System", $"Action`1", new string[] { "T" }),
-                _ => module.ImportCorlibReference("System", $"Action`{n}",
-                    Enumerable.Range(1, n).Select(i => $"T{i}").ToArray())
-            };
-        }
-
-        public static TypeReference Func(this ModuleDefinition module, int n = 0)
-        {
-            return n switch
-            {
-                0 => module.ImportCorlibReference("System", "Func`1", new string[] { "TResult" }),
-                1 => module.ImportCorlibReference("System", $"Func`2", new string[] { "T", "TResult" }),
-                _ => module.ImportCorlibReference("System", $"Func`{n}",
-                    Enumerable.Range(1, n).Select(i => $"T{i}").Concat(new string[] { "TResult" }).ToArray())
-            };
-        }
-
-        public static MethodReference TypeGetTypeFromHandle(this ModuleDefinition module)
-        {
-            var type = module.Type();
-            var mr = new MethodReference("GetTypeFromHandle", type, type)
-            {
-                HasThis = false
-            };
-            mr.Parameters.Add(new ParameterDefinition(module.RuntimeTypeHandle()));
-            return mr;
-        }
-
-        public static MethodReference TypeGetIsValueType(this ModuleDefinition module)
-        {
-            var type = module.Type();
-            var mr = new MethodReference("get_IsValueType", module.Bool(), type)
-            {
-                HasThis = true
-            };
-            return mr;
-        }
-
-        public static MethodReference TypeGetFullName(this ModuleDefinition module)
-        {
-            var type = module.Type();
-            var mr = new MethodReference("get_FullName", module.String(), type)
-            {
-                HasThis = true
-            };
-            return mr;
-        }
-
-        public static MethodReference StringEquals(this ModuleDefinition module)
-        {
-            var mr = new MethodReference("Equals", module.Bool(), module.String())
-            {
-                HasThis = false
-            };
-            mr.Parameters.Add(new ParameterDefinition(module.String()));
-            mr.Parameters.Add(new ParameterDefinition(module.String()));
-            return mr;
-        }
-
-        public static MethodReference ExtensionAttributeCtor(this ModuleDefinition module)
-        {
-            return new MethodReference(".ctor", module.Void(), module.ExtensionAttribute()) { HasThis = true };
-        }
-
-        public static MethodReference ParamArrayAttributeCtor(this ModuleDefinition module)
-        {
-            return new MethodReference(".ctor", module.Void(), module.ParamArrayAttribute()) { HasThis = true };
-        }
-
-        public static MethodReference FlagsAttributeCtor(this ModuleDefinition module)
-        {
-            return new MethodReference(".ctor", module.Void(), module.FlagsAttribute()) { HasThis = true };
-        }
-
-        public static MethodReference NotSupportedExceptionCtor(this ModuleDefinition module)
-        {
-            return new MethodReference(".ctor", module.Void(), module.NotSupportedException()) { HasThis = true, Parameters = { new ParameterDefinition(module.String()) } };
-        }
-
-        public static MethodReference ObsoleteAttributeCtor(this ModuleDefinition module)
-        {
-            return new MethodReference(".ctor", module.Void(), module.ObsoleteAttribute()) { HasThis = true, Parameters = { new ParameterDefinition(module.String()) } };
-        }
+    public static MethodReference ObsoleteAttributeCtor(this ModuleDefinition module)
+    {
+        return new MethodReference(".ctor", module.Void(), module.ObsoleteAttribute())
+        { HasThis = true, Parameters = { new ParameterDefinition(module.String()) } };
     }
 }
