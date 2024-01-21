452a748fa03dbabcdd5e2066641ed5a427d16db9
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2024-01-21 11:36:15 +1100

    Whitespace Fix?

--- Il2CppInterop.Runtime/Injection/InjectorHelpers.cs
+++ Il2CppInterop.Runtime/Injection/InjectorHelpers.cs
@@ -24,6 +24,7 @@ namespace Il2CppInterop.Runtime.Injection
         internal static Assembly Il2CppMscorlib = typeof(Il2CppSystem.Type).Assembly;
         internal static INativeAssemblyStruct InjectedAssembly;
         internal static INativeImageStruct InjectedImage;
+
         internal static ProcessModule Il2CppModule = Process.GetCurrentProcess()
             .Modules.OfType<ProcessModule>()
             .Single(x => s_assemblies.Contains(x.ModuleName, StringComparer.OrdinalIgnoreCase));
@@ -55,6 +56,7 @@ namespace Il2CppInterop.Runtime.Injection
         private static void CreateInjectedAssembly()
         {
             InjectedAssembly = UnityVersionHandler.NewAssembly();
+
             InjectedImage = UnityVersionHandler.NewImage();
 
             InjectedAssembly.Name.Name = Marshal.StringToHGlobalAnsi("InjectedMonoTypes");
@@ -141,29 +143,18 @@ namespace Il2CppInterop.Runtime.Injection
 
         private static long s_LastInjectedToken = -2;
         internal static readonly ConcurrentDictionary<long, IntPtr> s_InjectedClasses = new();
+
         /// <summary> (namespace, class, image) : class </summary>
         internal static readonly Dictionary<(string _namespace, string _class, IntPtr imagePtr), IntPtr> s_ClassNameLookup = new();
 
         #region Class::Init
+
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         internal delegate void d_ClassInit(Il2CppClass* klass);
+
         internal static d_ClassInit ClassInit;
 
-        private static readonly MemoryUtils.SignatureDefinition[] s_ClassInitSignatures =
-        {
-            new MemoryUtils.SignatureDefinition
-            {
-                pattern = "\xE8\x00\x00\x00\x00\x0F\xB7\x47\x28\x83",
-                mask = "x????xxxxx",
-                xref = true
-            },
-            new MemoryUtils.SignatureDefinition
-            {
-                pattern = "\xE8\x00\x00\x00\x00\x0F\xB7\x47\x48\x48",
-                mask = "x????xxxxx",
-                xref = true
-            }
-        };
+        private static readonly MemoryUtils.SignatureDefinition[] s_ClassInitSignatures = {new MemoryUtils.SignatureDefinition {pattern = "\xE8\x00\x00\x00\x00\x0F\xB7\x47\x28\x83", mask = "x????xxxxx", xref = true}, new MemoryUtils.SignatureDefinition {pattern = "\xE8\x00\x00\x00\x00\x0F\xB7\x47\x48\x48", mask = "x????xxxxx", xref = true}};
 
         private static d_ClassInit FindClassInit()
         {
@@ -188,6 +179,7 @@ namespace Il2CppInterop.Runtime.Injection
                 Logger.Instance.LogTrace("GameAssembly.dll: 0x{Il2CppModuleAddress}", Il2CppModule.BaseAddress.ToInt64().ToString("X2"));
                 throw new NotSupportedException("Failed to use signature for Class::Init and a substitute cannot be found, please create an issue and report your unity version & game");
             }
+
             nint pClassInit = s_ClassInitSignatures
                 .Select(s => MemoryUtils.FindSignatureInModule(Il2CppModule, s))
                 .FirstOrDefault(p => p != 0);
@@ -202,6 +194,8 @@ namespace Il2CppInterop.Runtime.Injection
 
             return Marshal.GetDelegateForFunctionPointer<d_ClassInit>(pClassInit);
         }
+
         #endregion
+
     }
 }
