57e5c835f0c1f896855b8da67ced2b0fddbe41ef
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-08-26 01:43:44 -0700

    ByReference

--- Il2CppInterop.Runtime/InteropTypes/ByReference.cs
+++ Il2CppInterop.Runtime/InteropTypes/ByReference.cs
@@ -1,85 +1,77 @@
 using System;
 using System.Runtime.CompilerServices;
-using Il2CppInterop.Runtime.Runtime;
 
 namespace Il2CppInterop.Runtime.InteropTypes;
 
-#pragma warning disable CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
-public unsafe struct ByReference<T>(T* value) : IIl2CppValueType, IIl2CppByReference
-    where T : IIl2CppValueType
+public unsafe struct ByReference<T>(void* pointer) : IIl2CppType<ByReference<T>>, IIl2CppByReference
+    where T : IIl2CppType<T>
 {
     static ByReference()
     {
         // Todo: set Il2CppClassPointerStore<ByReference<T>>.NativeClassPtr
     }
 
-    private T* _value = value;
+    private readonly void* _pointer = pointer;
 
-    readonly int IIl2CppByReference.ReferenceSize => typeof(T).IsValueType ? default(T)!.Size : IntPtr.Size;
+    public readonly bool IsNull => _pointer is null;
+
+    public readonly T? GetValue()
+    {
+        ThrowIfNull();
+        return Il2CppTypeHelper.ReadFromPointer<T>(_pointer);
+    }
+
+    public readonly void SetValue(T? value)
+    {
+        ThrowIfNull();
+        Il2CppTypeHelper.WriteToPointer(value, _pointer);
+    }
+
+    public readonly void* ToPointer() => _pointer;
+
+    private static int ReferenceSize => T.Size;
+
+    readonly int IIl2CppByReference.ReferenceSize => ReferenceSize;
 
     readonly nint IIl2CppByReference.ReferenceObjectClass => Il2CppClassPointerStore<T>.NativeClassPtr;
 
-    readonly int IIl2CppValueType.Size => IntPtr.Size;
+    static int IIl2CppType.Size => IntPtr.Size;
 
-    readonly nint IIl2CppValueType.ObjectClass => Il2CppClassPointerStore<ByReference<T>>.NativeClassPtr;
+    readonly nint IIl2CppType.ObjectClass => Il2CppClassPointerStore<ByReference<T>>.NativeClassPtr;
 
-    public static implicit operator ByReference<T>(T* value) => new(value);
-    public static implicit operator T*(ByReference<T> pointer) => pointer._value;
+    public static explicit operator ByReference<T>(void* value) => new(value);
+    public static explicit operator void*(ByReference<T> pointer) => pointer._pointer;
+
+    private readonly void ThrowIfNull()
+    {
+        if (_pointer is null)
+        {
+            throw new NullReferenceException($"Cannot access reference of type {typeof(T).Name} because it is null.");
+        }
+    }
 
     public static ByReference<T> FromRef(ref T value)
     {
-        return new ByReference<T>((T*)Unsafe.AsPointer(ref value));
+        return new ByReference<T>(Unsafe.AsPointer(ref value));
     }
 
     public static ref T ToRef(ByReference<T> value)
     {
-        return ref Unsafe.AsRef<T>(value._value);
+        return ref Unsafe.AsRef<T>(value._pointer);
     }
 
     readonly void IIl2CppByReference.WriteReferenceToSpan(Span<byte> span)
     {
-        if (typeof(T).IsValueType)
-        {
-            if (_value is null)
-            {
-                span.Slice(0, default(T)!.Size).Clear();
-            }
-            else
-            {
-                (*_value).WriteToSpan(span);
-            }
-        }
-        else
-        {
-            if (_value is null)
-            {
-                Il2CppTypeHelper.WritePointer(IntPtr.Zero, span);
-            }
-            else
-            {
-                Il2CppTypeHelper.WritePointer((*_value).Box(), span);
-            }
-        }
+        ThrowIfNull();
+        new ReadOnlySpan<byte>(_pointer, ReferenceSize).CopyTo(span);
     }
 
-    void IIl2CppByReference.ReadReferenceFromSpan(ReadOnlySpan<byte> span)
+    readonly void IIl2CppByReference.ReadReferenceFromSpan(ReadOnlySpan<byte> span)
     {
-        if (_value is null)
-        {
-            // Cannot read a reference into a null pointer
-        }
-        else if (typeof(T).IsValueType)
-        {
-            (*_value).ReadFromSpan(span);
-        }
-        else
-        {
-            var pointer = Il2CppTypeHelper.ReadPointer(span);
-            *_value = pointer == IntPtr.Zero ? default : (T?)Il2CppObjectPool.Get(pointer);
-        }
+        ThrowIfNull();
+        span.Slice(0, ReferenceSize).CopyTo(new Span<byte>(_pointer, ReferenceSize));
     }
 
-    readonly void IIl2CppValueType.WriteToSpan(Span<byte> span) => Il2CppTypeHelper.WritePointer((IntPtr)_value, span);
-    void IIl2CppValueType.ReadFromSpan(ReadOnlySpan<byte> span) => _value = (T*)Il2CppTypeHelper.ReadPointer(span);
+    static void IIl2CppType<ByReference<T>>.WriteToSpan(ByReference<T> value, Span<byte> span) => Il2CppTypeHelper.WritePointer((IntPtr)value._pointer, span);
+    static ByReference<T> IIl2CppType<ByReference<T>>.ReadFromSpan(ReadOnlySpan<byte> span) => (ByReference<T>)(void*)Il2CppTypeHelper.ReadPointer(span);
 }
-#pragma warning restore CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
