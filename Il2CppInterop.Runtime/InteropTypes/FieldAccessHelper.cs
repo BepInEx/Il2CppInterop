8870fd8aeeb30594c24caa2c7fe6d0b86c2e5339
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-08-29 00:20:53 -0700

    Allow wbarrior to be an implementation detail of field access

--- Il2CppInterop.Runtime/InteropTypes/FieldAccessHelper.cs
+++ Il2CppInterop.Runtime/InteropTypes/FieldAccessHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace Il2CppInterop.Runtime.InteropTypes;
 
@@ -6,7 +7,7 @@ public static unsafe class FieldAccessHelper
 {
     public static T? GetStaticFieldValue<T>(IntPtr fieldInfoPtr) where T : IIl2CppType<T>
     {
-        byte* data = stackalloc byte[T.Size];
+        var data = stackalloc byte[T.Size];
         IL2CPP.il2cpp_field_static_get_value(fieldInfoPtr, data);
         return T.ReadFromSpan(new ReadOnlySpan<byte>(data, T.Size));
     }
@@ -15,7 +16,7 @@ public static unsafe class FieldAccessHelper
     {
         if (typeof(T).IsValueType)
         {
-            byte* data = stackalloc byte[T.Size];
+            var data = stackalloc byte[T.Size];
             value.WriteToPointer(data);
             IL2CPP.il2cpp_field_static_set_value(fieldInfoPtr, data);
         }
@@ -27,13 +28,18 @@ public static unsafe class FieldAccessHelper
 
     public static T? GetInstanceFieldValue<T>(IIl2CppObjectBase instance, int fieldOffset) where T : IIl2CppType<T>
     {
-        byte* data = (byte*)instance.Pointer + fieldOffset;
+        var data = (byte*)instance.Pointer + fieldOffset;
         return Il2CppTypeHelper.ReadFromPointer<T>(data);
+    }
+
+    public static void SetInstanceFieldValue<T>(IIl2CppObjectBase instance, int fieldOffset, T? value) where T : IIl2CppType<T>
+    {
+        FunctionPointerCache<T>.SetInstanceFieldValue(instance, fieldOffset, value);
     }
 
     public static void SetInstanceFieldValue_Wbarrior<T>(IIl2CppObjectBase instance, int fieldOffset, T? value) where T : IIl2CppType<T>
     {
-        byte* data = (byte*)instance.Pointer + fieldOffset;
+        var data = (byte*)instance.Pointer + fieldOffset;
         if (typeof(T).IsValueType)
         {
             value!.WriteToPointer(data);
@@ -46,7 +52,7 @@ public static unsafe class FieldAccessHelper
 
     public static void SetInstanceFieldValue_Pointer<T>(IIl2CppObjectBase instance, int fieldOffset, T? value) where T : IIl2CppType<T>
     {
-        byte* data = (byte*)instance.Pointer + fieldOffset;
+        var data = (byte*)instance.Pointer + fieldOffset;
         if (typeof(T).IsValueType)
         {
             value!.WriteToPointer(data);
@@ -55,5 +61,25 @@ public static unsafe class FieldAccessHelper
         {
             *(IntPtr*)data = value.Box();
         }
+    }
+
+    private static bool HasWbarriorSupport()
+    {
+        if (NativeLibrary.TryLoad("GameAssembly", out var handle))
+        {
+            var result = NativeLibrary.TryGetExport(handle, "il2cpp_gc_wbarrier_set_field", out _);
+            NativeLibrary.Free(handle);
+            return result;
+        }
+        return false;
+    }
+
+    private static bool WbarriorSupport { get; } = HasWbarriorSupport();
+
+    private static class FunctionPointerCache<T> where T : IIl2CppType<T>
+    {
+        public static readonly delegate*<IIl2CppObjectBase, int, T?, void> SetInstanceFieldValue = WbarriorSupport
+            ? &FieldAccessHelper.SetInstanceFieldValue_Wbarrior
+            : &FieldAccessHelper.SetInstanceFieldValue_Pointer;
     }
 }
