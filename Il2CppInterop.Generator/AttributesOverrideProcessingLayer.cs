716c92862f53cab5c6379bddfbd91214831bed29
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-08-31 23:43:40 -0700

    Override method parameter attributes

--- Il2CppInterop.Generator/AttributesOverrideProcessingLayer.cs
+++ Il2CppInterop.Generator/AttributesOverrideProcessingLayer.cs
@@ -57,6 +57,19 @@ public class AttributesOverrideProcessingLayer : Cpp2IlProcessingLayer
                     method.OverrideAttributes = method.Attributes & ~FlagsToRemove;
 
                     method.OverrideImplAttributes = MethodImplAttributes.Managed;
+
+                    foreach (var parameter in method.Parameters)
+                    {
+                        // Todo: make a Cpp2IL pull request so that Cpp2IL respects HasDefault.
+                        // https://github.com/SamboyCoding/Cpp2IL/blob/d5260685fddb380f0ee884521d28c8309b2aff7e/Cpp2IL.Core/Model/Contexts/ParameterAnalysisContext.cs#L57
+                        // https://github.com/SamboyCoding/Cpp2IL/blob/d5260685fddb380f0ee884521d28c8309b2aff7e/Cpp2IL.Core/Model/Contexts/ParameterAnalysisContext.cs#L75-L78
+                        // https://github.com/SamboyCoding/Cpp2IL/blob/d5260685fddb380f0ee884521d28c8309b2aff7e/Cpp2IL.Core/Utils/AsmResolver/AsmResolverAssemblyPopulator.cs#L377-L386
+                        const ParameterAttributes ParamFlagsToRemove =
+                            ParameterAttributes.Optional |
+                            ParameterAttributes.HasDefault |
+                            ParameterAttributes.HasFieldMarshal;
+                        parameter.OverrideAttributes = parameter.Attributes & ~ParamFlagsToRemove;
+                    }
                 }
 
                 foreach (var field in type.Fields)
