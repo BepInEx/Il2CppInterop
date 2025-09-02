27e23b50e2af747ba8159ceba3ad66b05ca257a1
haolink <5476865+haolink@users.noreply.github.com>
2025-09-02 08:54:21 +0200

    Fix "Handle is not initialized" for Unity 2021.2.x games (#232)
    When the mono object is a System.Action object it seems GetGcHandlePtrFromIl2CppObject(pointer) returns a null pointer for the Garbage Collector Handle (or well IntPtr.Zero). This causes GCHandle.FromIntPtr(gcHandle) to throw an excepction.
    
    One solution after a lot of back and forth was to essentially check if the object class is a Delegate type class (which includes System.Action) and if so, get the garbage collector handle on the mTarget of the delegate object. This way in my testing the code will not interfere with other Unity versions - yet it allows to hook onto my test game - which happens to be a 2021.2.x game compiled with IL2CPP.
    
    In other Unity versions if (gcHandle == IntPtr.Zero) should be false so I doubt this would interfere in any way (it worked fine in other tested games for me).
    
    ----
    
    It's a hack but it seems to be working well, merging for v1.5.1. This code will be replaced in v2.0 with a proper fix.

--- Il2CppInterop.Runtime/Runtime/ClassInjectorBase.cs
+++ Il2CppInterop.Runtime/Runtime/ClassInjectorBase.cs
@@ -13,7 +13,28 @@ public static class ClassInjectorBase
     public static object GetMonoObjectFromIl2CppPointer(IntPtr pointer)
     {
         var gcHandle = GetGcHandlePtrFromIl2CppObject(pointer);
+
+        if (gcHandle == IntPtr.Zero) // The Garbage collector handle might return a null pointer
+        {
+            gcHandle = FallbackGetGcHandlePtrFromIl2CppDelegateMTarget(pointer);
+        }
+
         return GCHandle.FromIntPtr(gcHandle).Target;
+    }
+
+    /// <summary>
+    /// Tries to get the Garbage collector pointer from the m_target object from the m_target of the delegate.
+    /// Fixes Harmony in Unity 2021.2.x .
+    /// </summary>
+    private static IntPtr FallbackGetGcHandlePtrFromIl2CppDelegateMTarget(IntPtr pointer)
+    {
+        if (IL2CPP.il2cpp_class_is_assignable_from(Il2CppClassPointerStore<Il2CppSystem.MulticastDelegate>.NativeClassPtr, IL2CPP.il2cpp_object_get_class(pointer)))
+        {
+            var delegateObject = new Il2CppSystem.Delegate(pointer);
+            if (delegateObject.m_target != null && delegateObject.m_target.Pointer != IntPtr.Zero)
+                return GetGcHandlePtrFromIl2CppObject(delegateObject.m_target.Pointer);
+        }
+        return IntPtr.Zero;
     }
 
     public static unsafe IntPtr GetGcHandlePtrFromIl2CppObject(IntPtr pointer)
