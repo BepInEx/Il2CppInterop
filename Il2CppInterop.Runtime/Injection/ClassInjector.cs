7a1af59c5d6e447a5774e899138af28160cd6c3b
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2025-09-01 23:42:11 -0700

    Support prefixed types in ClassInjector.SystemTypeFromIl2CppType (#153)

--- Il2CppInterop.Runtime/Injection/ClassInjector.cs
+++ Il2CppInterop.Runtime/Injection/ClassInjector.cs
@@ -1147,7 +1147,9 @@ public static unsafe partial class ClassInjector
     internal static Type SystemTypeFromIl2CppType(Il2CppTypeStruct* typePointer)
     {
         var fullName = GetIl2CppTypeFullName(typePointer);
-        var type = Type.GetType(fullName) ?? throw new NullReferenceException($"Couldn't find System.Type for Il2Cpp type: {fullName}");
+        var type = Type.GetType(fullName)
+            ?? Type.GetType(fullName.Contains('.') ? "Il2Cpp" + fullName : "Il2Cpp." + fullName)
+            ?? throw new NullReferenceException($"Couldn't find System.Type for Il2Cpp type: {fullName}");
 
         INativeTypeStruct wrappedType = UnityVersionHandler.Wrap(typePointer);
         if (wrappedType.Type == Il2CppTypeEnum.IL2CPP_TYPE_GENERICINST)
