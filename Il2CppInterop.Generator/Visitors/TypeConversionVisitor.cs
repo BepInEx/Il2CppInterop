cb803eb6888387544dba279478a6df2082c11d40
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-10-22 14:01:51 -0700

    Treat multi dimensional arrays with rank 1 as sz arrays

--- Il2CppInterop.Generator/Visitors/TypeConversionVisitor.cs
+++ Il2CppInterop.Generator/Visitors/TypeConversionVisitor.cs
@@ -34,24 +34,6 @@ internal sealed class TypeConversionVisitor : TypeReplacementVisitor
             ("Il2CppSystem.Object", "Il2CppSystem.IObject"),
             ("Il2CppSystem.Enum", "Il2CppSystem.IEnum"),
             ("Il2CppSystem.ValueType", "Il2CppSystem.IValueType"),
-            //("Il2CppSystem.Object", "System.Object"),
-            //("Il2CppSystem.Void", "System.Void"),
-            //("Il2CppSystem.Enum", "System.Enum"),
-            //("Il2CppSystem.ValueType", "System.ValueType"),
-            //("Il2CppSystem.Boolean", "System.Boolean"),
-            //("Il2CppSystem.Char", "System.Char"),
-            //("Il2CppSystem.SByte", "System.SByte"),
-            //("Il2CppSystem.Byte", "System.Byte"),
-            //("Il2CppSystem.Int16", "System.Int16"),
-            //("Il2CppSystem.UInt16", "System.UInt16"),
-            //("Il2CppSystem.Int32", "System.Int32"),
-            //("Il2CppSystem.UInt32", "System.UInt32"),
-            //("Il2CppSystem.Int64", "System.Int64"),
-            //("Il2CppSystem.UInt64", "System.UInt64"),
-            //("Il2CppSystem.Single", "System.Single"),
-            //("Il2CppSystem.Double", "System.Double"),
-            //("Il2CppSystem.IntPtr", "System.IntPtr"),
-            //("Il2CppSystem.UIntPtr", "System.UIntPtr"),
         ];
 
         var replacementDictionary = replacements.ToDictionary(pair => il2CppMscorlib.GetTypeByFullNameOrThrow(pair.Item1), pair => il2CppMscorlib.GetTypeByFullNameOrThrow(pair.Item2));
@@ -70,6 +52,7 @@ internal sealed class TypeConversionVisitor : TypeReplacementVisitor
     {
         return type.Rank switch
         {
+            1 => Il2CppArrayBase.MakeGenericInstanceType([elementResult]),
             2 => ArrayRank2.MakeGenericInstanceType([elementResult]),
             3 => ArrayRank3.MakeGenericInstanceType([elementResult]),
             _ => throw new NotImplementedException($"Support for arrays with rank {type.Rank} has not been implemented."),
