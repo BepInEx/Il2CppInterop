b1208fade93481f787cf8f7a025953bf0617f6b2
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-11-22 13:44:56 -0800

    Move MethodAnalysisContextExtensions

--- Il2CppInterop.Generator/Extensions/MethodAnalysisContextExtensions.cs
+++ Il2CppInterop.Generator/Extensions/MethodAnalysisContextExtensions.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Cpp2IL.Core.Model.Contexts;
 
-namespace Il2CppInterop.Generator;
+namespace Il2CppInterop.Generator.Extensions;
 
 internal static class MethodAnalysisContextExtensions
 {
