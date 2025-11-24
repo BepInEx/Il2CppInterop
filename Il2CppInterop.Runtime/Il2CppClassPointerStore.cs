76290ec1f85fb04782e248ae2ee1b94e097c1422
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-11-24 13:17:10 -0800

    Update Il2CppClassPointerStore * Remove support for System primitives * Remove support for enums * Remove CreatedTypeRedirect * Correctly handle IObject, IValueType, IEnum

--- Il2CppInterop.Runtime/Il2CppClassPointerStore.cs
+++ Il2CppInterop.Runtime/Il2CppClassPointerStore.cs
@@ -1,83 +1,57 @@
-using System;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using Il2CppInterop.Common.Attributes;
 using Il2CppInterop.Runtime.Attributes;
-using String = Il2CppSystem.String;
-using Void = Il2CppSystem.Void;
+using Il2CppSystem;
 
 namespace Il2CppInterop.Runtime;
 
 public static class Il2CppClassPointerStore
 {
-    public static IntPtr GetNativeClassPointer(Type type)
+    public static nint GetNativeClassPointer(System.Type type)
     {
-        if (type == typeof(void)) return Il2CppClassPointerStore<Void>.NativeClassPtr;
-        if (type == typeof(String)) return Il2CppClassPointerStore<string>.NativeClassPtr;
-        return (IntPtr)typeof(Il2CppClassPointerStore<>)
+        return (nint)typeof(Il2CppClassPointerStore<>)
             .MakeGenericType(type)
-            .GetField(nameof(Il2CppClassPointerStore<int>.NativeClassPtr))!
+            .GetField(nameof(Il2CppClassPointerStore<>.NativeClassPtr))!
             .GetValue(null)!;
     }
 
-    internal static void SetNativeClassPointer(Type type, IntPtr value)
+    internal static void SetNativeClassPointer(System.Type type, nint value)
     {
         typeof(Il2CppClassPointerStore<>)
             .MakeGenericType(type)
-            .GetField(nameof(Il2CppClassPointerStore<int>.NativeClassPtr))!
+            .GetField(nameof(Il2CppClassPointerStore<>.NativeClassPtr))!
             .SetValue(null, value);
     }
 }
 
 public static class Il2CppClassPointerStore<T>
 {
-    public static IntPtr NativeClassPtr;
-    public static Type? CreatedTypeRedirect;
+    public static nint NativeClassPtr;
 
     static Il2CppClassPointerStore()
     {
-        var targetType = typeof(T);
-        if (!targetType.IsEnum)
+        if (typeof(T) == typeof(IObject))
         {
-            RuntimeHelpers.RunClassConstructor(targetType.TypeHandle);
+            NativeClassPtr = Il2CppClassPointerStore<Object>.NativeClassPtr;
+        }
+        else if (typeof(T) == typeof(IValueType))
+        {
+            NativeClassPtr = Il2CppClassPointerStore<ValueType>.NativeClassPtr;
+        }
+        else if (typeof(T) == typeof(IEnum))
+        {
+            NativeClassPtr = Il2CppClassPointerStore<Enum>.NativeClassPtr;
         }
         else
         {
-            var assemblyName = targetType.Module.Name;
-            var @namespace = targetType.Namespace;
-            var name = targetType.Name;
-            foreach (var customAttribute in targetType.CustomAttributes)
+            RuntimeHelpers.RunClassConstructor(typeof(T).TypeHandle);
+            foreach (var customAttribute in typeof(T).CustomAttributes)
             {
-                if (customAttribute.AttributeType != typeof(OriginalNameAttribute)) continue;
-                assemblyName = (string?)customAttribute.ConstructorArguments[0].Value;
-                @namespace = (string?)customAttribute.ConstructorArguments[1].Value;
-                name = (string?)customAttribute.ConstructorArguments[2].Value;
+                if (customAttribute.AttributeType != typeof(AlsoInitializeAttribute)) continue;
+
+                var linkedType = (System.Type?)customAttribute.ConstructorArguments[0].Value;
+                if (linkedType is not null)
+                    RuntimeHelpers.RunClassConstructor(linkedType.TypeHandle);
             }
-            assemblyName ??= "";
-            @namespace ??= "";
-            name ??= "";
-
-            if (targetType.IsNested)
-                NativeClassPtr =
-                    IL2CPP.GetIl2CppNestedType(Il2CppClassPointerStore.GetNativeClassPointer(targetType.DeclaringType!),
-                        name);
-            else
-                NativeClassPtr =
-                    IL2CPP.GetIl2CppClass(assemblyName, @namespace, name);
-        }
-
-        if (targetType.IsPrimitive || targetType == typeof(string))
-            RuntimeHelpers.RunClassConstructor(AppDomain.CurrentDomain.GetAssemblies()
-                .Single(it => it.GetName().Name == "Il2Cppmscorlib").GetType("Il2Cpp" + targetType.FullName)!
-                .TypeHandle);
-
-        foreach (var customAttribute in targetType.CustomAttributes)
-        {
-            if (customAttribute.AttributeType != typeof(AlsoInitializeAttribute)) continue;
-
-            var linkedType = (Type?)customAttribute.ConstructorArguments[0].Value;
-            if (linkedType is not null)
-                RuntimeHelpers.RunClassConstructor(linkedType.TypeHandle);
         }
     }
 }
