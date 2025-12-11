8c146903392eca7f4c77235792d19c1559f47d42
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-12-11 14:00:42 -0800

    Parameter type validation in icall resolution

--- Il2CppInterop.Runtime/IL2CPP.cs
+++ Il2CppInterop.Runtime/IL2CPP.cs
@@ -339,7 +339,7 @@ public static unsafe partial class IL2CPP
             var parameterType = parameter.ParameterType;
 
             // Parameter is a ByReference<T>, and we need to get the underlying type T
-            var elementType = parameterType.GetElementType()!;
+            var elementType = parameterType.GetElementType() ?? throw new InvalidOperationException("ICall parameters must be ByReference<T> types");
 
             var elementSize = (int)sizeOfMethod.MakeGenericMethod(elementType).Invoke(null, null)!;
 
