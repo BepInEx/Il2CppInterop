1161e55c5ffa9ac11d3745cb65b665afd7776c69
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-12-16 12:42:43 -0800

    Move Il2CppInterfaceCollection to a new file

--- Il2CppInterop.Runtime/Injection/ClassInjector.cs
+++ Il2CppInterop.Runtime/Injection/ClassInjector.cs
@@ -20,39 +20,6 @@ using Void = Il2CppSystem.Void;
 
 namespace Il2CppInterop.Runtime.Injection;
 
-public unsafe class Il2CppInterfaceCollection : List<INativeClassStruct>
-{
-    public Il2CppInterfaceCollection(IEnumerable<INativeClassStruct> interfaces) : base(interfaces)
-    {
-    }
-
-    public Il2CppInterfaceCollection(IEnumerable<Type> interfaces) : base(ResolveNativeInterfaces(interfaces))
-    {
-    }
-
-    private static IEnumerable<INativeClassStruct> ResolveNativeInterfaces(IEnumerable<Type> interfaces)
-    {
-        return interfaces.Select(it =>
-        {
-            var classPointer = Il2CppClassPointerStore.GetNativeClassPointer(it);
-            if (classPointer == IntPtr.Zero)
-                throw new ArgumentException(
-                    $"Type {it} doesn't have an IL2CPP class pointer, which means it's not an IL2CPP interface");
-            return UnityVersionHandler.Wrap((Il2CppClass*)classPointer);
-        });
-    }
-
-    public static implicit operator Il2CppInterfaceCollection(INativeClassStruct[] interfaces)
-    {
-        return new(interfaces);
-    }
-
-    public static implicit operator Il2CppInterfaceCollection(Type[] interfaces)
-    {
-        return new(interfaces);
-    }
-}
-
 public static unsafe partial class ClassInjector
 {
     /// <summary> type.FullName </summary>
@@ -136,7 +103,7 @@ public static unsafe partial class ClassInjector
         Il2CppInterfaceCollection interfaces;
         {
             var interfacesAttribute = type.GetCustomAttribute<Il2CppImplementsAttribute>();
-            interfaces = interfacesAttribute?.Interfaces ?? Array.Empty<Type>();
+            interfaces = interfacesAttribute?.Interfaces ?? [];
         }
 
         if (type == null)
