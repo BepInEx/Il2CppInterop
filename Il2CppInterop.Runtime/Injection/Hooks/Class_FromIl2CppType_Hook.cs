f8b4e49fe8b74824c0530e6b4e6fafb0dfff0147
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-22 15:39:37 -0700

    Refactor Class_FromIl2CppType_Hook

--- Il2CppInterop.Runtime/Injection/Hooks/Class_FromIl2CppType_Hook.cs
+++ Il2CppInterop.Runtime/Injection/Hooks/Class_FromIl2CppType_Hook.cs
@@ -30,7 +30,7 @@ namespace Il2CppInterop.Runtime.Injection.Hooks
             if (type == null)
                 return Original(type, throwOnError);
 
-            if ((nint)type->data < 0 && (type->type == Il2CppTypeEnum.IL2CPP_TYPE_CLASS || type->type == Il2CppTypeEnum.IL2CPP_TYPE_VALUETYPE))
+            if ((nint)type->data < 0 && (type->type is Il2CppTypeEnum.IL2CPP_TYPE_CLASS or Il2CppTypeEnum.IL2CPP_TYPE_VALUETYPE))
             {
                 InjectorHelpers.s_InjectedClasses.TryGetValue((nint)type->data, out var classPointer);
                 return (Il2CppClass*)classPointer;
