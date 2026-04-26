5290291e8ce389b590c54c8bb6ea47d05a08443b
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-25 22:17:11 -0700

    Fix nullable warnings for startup classes

--- Il2CppInterop.Runtime/Startup/RuntimeConfiguration.cs
+++ Il2CppInterop.Runtime/Startup/RuntimeConfiguration.cs
@@ -5,6 +5,6 @@ namespace Il2CppInterop.Runtime.Startup;
 
 public record RuntimeConfiguration
 {
-    public Version UnityVersion { get; init; }
-    public IDetourProvider DetourProvider { get; init; }
+    public required Version UnityVersion { get; init; }
+    public required IDetourProvider DetourProvider { get; init; }
 }
